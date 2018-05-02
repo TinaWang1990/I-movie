c438487352d74120ee53f99349729bf0f417ce5e
TinaWang <shihuiwang1990@gmail.com>
2018-05-02 23:20:38 +0000

    1

---
+++ header.php
@@ -0,0 +1,17 @@
+<div class="bg-dark container " style="margin: 0; width: 100vw; height: 50px">
+  			
+  			<div class="row bg-dark text-white text-center" style="margin: 0; width: 100vw; height: 50px; line-height: 50px" >
+
+  				<div class="col-2 text-center">I-Movie</div>
+  				<div class="col-5 mt-2">
+  					<form class="form-inline">
+   					 <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+    				<button class="btn btn-outline-success my-1 my-sm-0 bg-white" type="submit"><img src="./search.PNG"></button>
+ 					 </form>
+  				</div>
+  				<div class="col-1">Home</div>
+  				<div class="col-1">Movie</div>
+  				<div class="col-1">Comment</div>
+  				<div class="col-1">Login</div>
+  			</div>
+  		</div>
---
+++ index.php
@@ -0,0 +1,120 @@
+
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+
+    <title>I-Movie</title>
+    <style type="text/css">
+    body{
+    	margin: 0;
+    	padding: 0;
+    }
+    .carousel-item{
+    	width: 100vw;
+    	padding: 0;
+    	height: 60vh;
+    }
+    .carousel-item img{
+    	width: 100%;
+    	height:100%;
+    }
+    .H2 {
+    	padding: 0;
+    	height: 20vh;
+    	width: 80vw;
+    	position: fixed; top:48vh; left: 20vw;
+    }
+    .H2 row{
+    	padding: 0;
+    	margin: 0;
+    }
+    .H2 img{
+
+    	width: 10vw;
+    	height: 25vh;
+    	
+    }
+    	
+    </style>
+  </head>
+  <body>
+    <div class="d-flex flex-column">
+  		<div class="bg-dark container " style="margin: 0; width: 100vw; height: 50px">
+  			
+  			<div class="row bg-dark text-white text-center" style="margin: 0; width: 100vw; height: 50px; line-height: 50px" >
+
+  				<div class="col-4 text-center">I-Movie</div>
+  				<div class="col-3 mt-2">
+  					
+  				</div>
+  				<div class="col-1">Home</div>
+  				<div class="col-1">Movie</div>
+  				<div class="col-1">Comment</div>
+  				<div class="col-1">Login</div>
+  			</div>
+  		</div>
+
+
+  		<div class="H1">
+  			<div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+ 			 <div class="carousel-inner">
+   			 <div class="carousel-item active">
+      		<img class="d-block w-100 h-30" src="./ready background.jpg" alt="First slide">
+    		</div>
+    		<div class="carousel-item">
+     		 <img class="d-block w-100 h-30" src="./maze runner-back.jpg" alt="Second slide">
+    		</div>
+    		<div class="carousel-item">
+     		<img class="d-block w-100 h-30" src="./rampage-back.jpg" alt="Third slide">
+    		</div>
+  			</div>
+			</div>
+			<div class=" " style="position: absolute; top:30vh; left: 25vw;width: 60vw; ">
+  					<form class="form-inline">
+   					 <input class="form-control mr-sm-2 " type="search" placeholder="Search" aria-label="Search" style="width: 40vw;height: 5vh;">
+    				<button class="btn btn-outline-success my-1 my-sm-0 bg-white" type="submit" style="height: 5vh;"><img src="./search.PNG"></button>
+ 					 </form>
+  				</div>
+  			<div class="H2 container m-0" style="padding:0; " >
+  			<div class="row">
+  				<div class="col-2.4"><img src="./ready player one poster.jpg" style=""></div>
+  				<div class="col-2.4"><img src="./ready player one poster.jpg" style=""></div>
+  				<div class="col-2.4"><img src="./ready player one poster.jpg" style=""></div>
+  				<div class="col-2.4"><img src="./star wars-poster.jpg" style=""></div>
+  				<div class="col-2.4"><img src="./ready player one poster.jpg" style=""></div>
+  				
+  			</div>
+
+  		</div>
+
+    	
+  			
+  		
+	</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js" ></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+  </body>
+</html>
